d6e24689f69d9167cccbd214f3304174c0794702
Salo <salomonco@gmail.com>
2022-11-29 16:34:18 -0500

    challenge 8 - homeworld

--- src/components/StarWars.js
+++ src/components/StarWars.js
@@ -3,46 +3,82 @@ import "../App.css";
 
 
 function StarWars(){
-
-  // ---------------------------- Constants
+  // ---------------------------- Constants ----------------------
   const [value, setValue] = useState("");
-const [data, setData] = useState("");
-  // ---------------------------- Constants
+  const [data, setData] = useState("");
+  const [home, setHome] = useState("")
+  const charList = [];
+
+  // ---------------------------- Constants ----------------------
+
+  // ---------------------------- Helper Function ---------------
+
+    function setList(char){
+        charList.push(char)
+    }
+
+
+  // ---------------------------- Helper Function ---------------
 
   // ----------------------- API CALL ---------------
   async function getCharacter() {
     const path = `https://swapi.dev/api/people/${value}/`;
-    const res = await fetch(path)
-    const json = await res.json()
-    console.log(value)
-    console.log(json)
-    setData(json)
+    const res = await fetch(path);
+    const json = await res.json();
+
+    setData(json);
+
+    const homepath = await json.homeworld
+    const homeRes = await fetch(homepath)
+    const homeJson = await homeRes.json()
+
+    console.log(homepath)
+
+    setHome(homeJson)
   }
 
   // ----------------------- API CALL ---------------
 
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          getCharacter();
-        }}
-      >
-        <input
-          type="number"
-          min="1"
-          max="83"
-          placeholder="Write a Number 1-83"
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <h1>Character Stats:</h1>
-      <h2>{data.name}</h2>
-      <h2>{data.height}</h2>
-      <h2>{data.mass}</h2>
-      <h2>{data.eye_color}</h2>
+        <div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            getCharacter();
+          }}
+        >
+          <input
+            type="number"
+            min="1"
+            max="83"
+            placeholder="Write a Number 1-83"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+          <h2>Character List</h2>
+          <h3>{charList}</h3>
+
+      </div>
+      </div>
+      <div>
+        <h1>Character Stats:</h1>
+        <h2>Name: {data.name}</h2>
+        <h2>Height: {data.height}</h2>
+        <h2>Mass: {data.mass}</h2>
+        <h2>Eye Color: {data.eye_color}</h2>
+
+        <h1>Character Homeworld:</h1>
+        <h2>Name: {home.name}</h2>
+        <h2>Terrain: {home.terrain}</h2>
+        <h2>Population: {home.population}</h2>
+
+
+      </div>
     </div>
   );
 }
